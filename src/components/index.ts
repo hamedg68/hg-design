c6fcd3b44303ad8f55e7942732ec3b2981698417
hamedPC <hamedgorji1368@gmail.com>
2023-09-24 19:07:00 +0330

    feat: add selection component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,3 @@
 export { default as TextInput } from './TextInput.vue';
-export { default as iinput } from './iinput.vue';
 export { default as DatePicker } from './DatePicker.vue';
+export { default as SelectionInput } from './SelectionInput.vue';
--- src/components/main.ts
+++ src/components/main.ts
@@ -1,11 +1,12 @@
 import type { App } from "vue";
-import { DatePicker, TextInput } from "@/components";
-import '../style.css'
+import { DatePicker, TextInput, SelectionInput } from "@/components";
+import "../style.css";
 export default {
   install: (app: App) => {
     app.component("DatePicker", DatePicker);
     app.component("TextInput", TextInput);
+    app.component("SelectionInput", SelectionInput);
   },
 };
 
-export { DatePicker, TextInput };
+export { DatePicker, TextInput, SelectionInput };
