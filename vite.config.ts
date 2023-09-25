6147d1f6c28622d499a127b6994052b0ca92b553
Hamed Gorji <hamedgorji1368@gmail.com>
2023-09-25 10:46:13 +0330

    feat: config for data picker

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,6 @@ import typescript2 from "rollup-plugin-typescript2";
 import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  optimizeDeps: {
-    exclude: ["vue3-persian-datetime-picker"],
-  },
   plugins: [
     vue(),
     dts({
@@ -53,6 +50,7 @@ export default defineConfig({
         exports: "named",
         globals: {
           vue: "Vue",
+          'vue3-persian-datetime-picker' : "Vue3PersianDatetimePicker"
         },
       },
     },
